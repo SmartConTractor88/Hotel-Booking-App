824f7dc4f6ef01ec190d0b10917e1fb60dd5332f
SmartConTractor <login.blockchain888@gmail.com>
2025-03-21 19:14:24 +0100

    Add steps for booking. Verify credit cards.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("hotels.csv", encoding="ISO-8859-1", dtype={"id": str})
 
+df_cards = pd.read_csv("cards.csv", encoding="ISO-8859-1", dtype=str).to_dict(orient="records")
+
+df_card_security = pd.read_csv("card_security.csv", encoding="ISO-8859-1", dtype=str)
+
 class Hotel:
 
     def __init__(self, hotel_id):
@@ -44,6 +48,32 @@ class ReservationTicket:
         Enjoy your stay!!!
         """
         return content
+    
+
+class CreditCard:
+    
+    def __init__(self, number):
+
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        
+        card_data = {"number":self.number, "expiration": expiration, 
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else: 
+            return False
+        
+
+class SecureCreditCard(CreditCard): # inheritance
+
+    def authenticate(self, given_password):
+        password = df_card_security.loc[df_card_security["number"] == self.number, "password"].squeeze()
+        if str(password) == str(given_password):
+            return True
+        else:
+            return False
 
 print(df)
 
@@ -52,13 +82,24 @@ hotel_ID: str = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
 
-    nights = int(input("How long would you like to stay(nights)? : "))
-    name = input("What is your name? : ")
+    credit_card = SecureCreditCard(number="1234")
 
-    ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(ticket.generate())
+    if credit_card.validate(expiration="12/26", holder="ALBERT BRICK", cvc="123"):
+        user_pass = input("Enter your password: ")
+        if credit_card.authenticate(given_password=str(user_pass)):
+
+            hotel.book()
+
+            nights = int(input("How long would you like to stay(nights)? : "))
+            name = input("What is your name? : ")
+
+            ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(ticket.generate())
+        else:
+            print("Password incorrect.")
+    else:
+        print("Invalid card data.")
 
 else:
     print("Hotel not avalable")
